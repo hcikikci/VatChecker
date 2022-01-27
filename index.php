7629dcca6a998f4d1ea2c9dcf1a09e7b92c42fd8
Halitcan Çıkıkçı <halitcancikikci@gmail.com>
2022-01-27 14:58:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
  * Author: Halitcan Çıkıkçı
  * Author URI: https://github.com/hcikikci
  * Description: Automatically checks vat number of customer and if vat number is valid, set the user vat exempt.
- * Version: 0.1.0
- * License: 0.1.0
+ * Version: 0.2.0
+ * License: 0.2.0
  * License URL: http://www.gnu.org/licenses/gpl-2.0.txt
  * text-domain: vatchecker
  * GitHub Plugin URI: https://github.com/hcikikci/VatChecker
